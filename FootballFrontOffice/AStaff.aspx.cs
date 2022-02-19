e8e2ae75afb7e478577418efda7d31452d53d461
Suhayb Pirji <P2597781@my365.dmu.ac.uk>
2022-02-19 21:20:39 +0000

    Good

--- FootballFrontOffice/AStaff.aspx.cs
+++ FootballFrontOffice/AStaff.aspx.cs
@@ -24,4 +24,27 @@ public partial class AStaff : System.Web.UI.Page
         //redirect to the viewer page
         Response.Redirect("StaffViewer.aspx");
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //create an instance of the staff class
+        clsStaff AStaff = new clsStaff();
+        //variable to store the primary key
+        Int32 StaffNo;
+        //variable to store the result of the find operation
+        Boolean Found = false;
+        //get the primary key entered by the user
+        StaffNo = Convert.ToInt32(txtStaffNo.Text);
+        //find the record
+        Found = AStaff.Find(StaffNo);
+        //if found
+        if (Found == true)
+        {
+            //display the values of the properties in the form
+            txtFirstName.Text = AStaff.FirstName;
+            txtSurname.Text = AStaff.Surname;
+            txtIncome.Text = AStaff.Income.ToString();
+            txtDateAdded.Text = AStaff.DateAdded.ToString();
+        }
+    }
 }
