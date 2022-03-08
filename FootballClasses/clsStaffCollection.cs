98e9c96b4e036fc4d3f252712177783476bb83c9
Suhayb Pirji <P2597781@my365.dmu.ac.uk>
2022-03-08 12:06:29 +0000

    Good

---
+++ FootballClasses/clsStaffCollection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FootballClasses
+{
+    public class clsStaffCollection
+    {
+        //prvate data member for the list
+        List<clsStaff> mStaffList = new List<clsStaff>();
+
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                //return the private data
+                return mStaffList;
+            }
+            set
+            {
+                //set the private data
+                mStaffList = value;
+            }
+        }
+        //public property for count
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mStaffList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+        public clsStaff ThisStaff { get; set; }
+
+        //constructor for the class
+        public clsStaffCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc-tblStaff_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank staff record
+                clsStaff AStaff = new clsStaff();
+                //read in the fields from the current record
+                AStaff.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                AStaff.StaffNo = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffNo"]);
+                AStaff.FirstName = Convert.ToString(DB.DataTable.Rows[Index]["FirstName"]);
+                AStaff.Surname = Convert.ToString(DB.DataTable.Rows[Index]["Surname"]);
+                AStaff.Income = Convert.ToDouble(DB.DataTable.Rows[Index]["Income"]);
+                AStaff.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+                //add the record to the private data member
+                mStaffList.Add(AStaff);
+                //point at the next record
+                Index++;
+            }
+        }
+    }
+}
--- FootballFrontOffice/Default.aspx.cs
+++ FootballFrontOffice/Default.aspx.cs
@@ -1,14 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using FootballClasses;
 
-public partial class _Default : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
+   public partial class _Default : System.Web.UI.Page
+   {
+        //this function handles the load event for the page
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayStaff();
+        }
     }
-}
+
+    void DisplayStaff()
+    {
+        //create an instance of the staff collection
+        FootballClasses.clsStaffCollection Staff = new FootballClasses.clsStaffCollection();
+        //set the data source to the list of staff in the collection
+        lstStaff.DataSource = Staff.StaffList;
+        //set the name of the primary key
+        lstStaff.DataValueField = "StaffNo";
+        //set the data field to display
+        lstStaff.DataValueField = "FirstName";
+        //bind the data to the list
+        lstStaff.DataBind();
+    }
+  }
