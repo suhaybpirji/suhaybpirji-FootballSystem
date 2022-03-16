373dd3b73ca9917e22aeedd46eb1e468babb5317
P2611172 <P2611172@GWHXX281WIN017W>
2022-03-16 16:03:27 +0000

    I have done week 19

---
+++ FootballTesting/tstOrder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FootballTesting
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //test to see that it exists
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+
+    class clsOrder
+    {
+    }
+}
