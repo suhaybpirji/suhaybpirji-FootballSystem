44e6974e07ce6fe44f6ace3625da8cd3e77ff48c
P2623705 <P2623705@my365.dmu.ac.uk>
2022-03-23 22:49:54 +0000

    Supplier ASPX.

---
+++ FootballClasses/clsSupplierCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballClasses
+{
+    public class tstSupplierCollection
+   
+    {
+    }
+    [TestMethod]
+    public void InstanceOK()
+    {
+        clsSupplierCollection AllSuppliers = new clsSupplierCollection();
+        Assert.IsNotNull(AllSuppliers);
+    }
+}
