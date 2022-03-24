989e5fd55cf6088cdb0a090e6e583f196cc50d64
P2623705 <P2623705@my365.dmu.ac.uk>
2022-03-24 15:09:58 +0000

    Testing

--- FootballClasses/clsSupplierCollection.cs
+++ FootballClasses/clsSupplierCollection.cs
@@ -1,19 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FootballClasses
 {
-    public class tstSupplierCollection
-   
+    public class clsSupplierCollection
     {
+        List<clsSupplier> mSuppliersList = new List<clsSupplier>();
+        public List<clsSupplier> SuppliersList
+        {
+            get
+            {
+                return mSuppliersList;
+
+            }
+            set
+            {
+                mSuppliersList = value;
+
+            }
+
+        }
+        public int Count
+
+        {
+            get
+            {
+                return mSuppliersList.Count;
+            }
+            set
+            {
+
+            }
+
+        }
+
+
+
+
+        public clsSupplierCollection()
+        {
+            clsSupplier TestItem = new clsSupplier();
+            TestItem.Active = true;
+            TestItem.DateAdded = System.DateTime.Now.Date;
+            TestItem.PostCode = "LE1 6RL";
+
+            TestItem = new clsSupplier();
+
+            TestItem.Active = true;
+            TestItem.DateAdded = System.DateTime.Now.Date;
+            TestItem.PostCode = "LE1 5TY";
+
+        }
+
+
+
+
+        public clsSupplier ThisSupplier { get; set; }
     }
-    [TestMethod]
-    public void InstanceOK()
+
+    public clsSupplierCollection()
     {
-        clsSupplierCollection AllSuppliers = new clsSupplierCollection();
-        Assert.IsNotNull(AllSuppliers);
+
+        Int32 Index = 0;
+        Int32 RecordCount = 0;
+        clsDataConnection DB = new clsDataConnection();
+        DB.Execute("Sproc_tblSupplier_SelectAll");
+        RecordCount = DB.Count;
+        while (Index < RecordCount)
+        {
+            clsSupplier ASupplier = new clsSupplier();
+            ASupplier.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+            ASupplier.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+            ASupplier.PostCode = Convert.ToString(DB.DataTable.Rows[Index]["PostCode"]);
+            mSuppliersList.Add(ASupplier);
+            Index++;
+
+
+
     }
+  }
 }
